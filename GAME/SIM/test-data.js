26c8a9560378c5f06a55a58f37402fb2849bfdd0
Ollie Farrelly <oliverfarrelly06@gmail.com>
2026-09-16 20:35:03 +1200

    d

--- GAME/SIM/test-data.js
+++ GAME/SIM/test-data.js
@@ -1,3 +1,10 @@
+function findPlayer(name, squad) {
+  const matches = playerRatingsMaster.filter(p => p.Player.includes(name));
+  const player = squad ? matches.find(p => p.Squad === squad) : matches[0];
+  if (!player) console.warn(`Could not find player: ${name}`);
+  return player;
+}
+
 function convertToTeamStrengthFormat(realPlayer, slotSubPosition) {
   return {
     name: realPlayer.Player,
@@ -7,12 +14,19 @@ function convertToTeamStrengthFormat(realPlayer, slotSubPosition) {
   };
 }
 
-
-
-const salah = playerRatingsMaster.find(p => p.Player.includes("Salah"));
-
 const realXI = [
-  convertToTeamStrengthFormat(salah, "Right Winger")
+  convertToTeamStrengthFormat(findPlayer("Alisson"), "Goalkeeper"),
+  convertToTeamStrengthFormat(findPlayer("van Dijk"), "Centre-Back"),
+  convertToTeamStrengthFormat(findPlayer("Saliba"), "Centre-Back"),
+  convertToTeamStrengthFormat(findPlayer("Alexander-Arnold"), "Right-Back"),
+  convertToTeamStrengthFormat(findPlayer("Robertson"), "Left-Back"),
+  convertToTeamStrengthFormat(findPlayer("Rodri", "Manchester City"), "Defensive Midfield"),
+  convertToTeamStrengthFormat(findPlayer("Bellingham"), "Central Midfield"),
+  convertToTeamStrengthFormat(findPlayer("degaard"), "Attacking Midfield"),
+  convertToTeamStrengthFormat(findPlayer("Salah"), "Right Winger"),
+  convertToTeamStrengthFormat(findPlayer("Haaland"), "Centre-Forward"),
+  convertToTeamStrengthFormat(findPlayer("Vinicius"), "Left Winger")
 ];
 
 console.log(realXI);
+console.log(computeTeamStrength(realXI));
--- PIPELINE/player_ratings_master.R
+++ PIPELINE/player_ratings_master.R
@@ -1,3 +1,6 @@
+library(tidyverse)
+
+
 gk_final  <- gk_ratings      |> select(Player, Squad, sub_position, Min, market_value_in_eur, final_rating)
 fwd_final <- forward_ratings |> select(Player, Squad, sub_position, Min, market_value_in_eur, final_rating)
 cb_final  <- cb_ratings      |> select(Player, Squad, sub_position, Min, market_value_in_eur, final_rating)
@@ -12,3 +15,11 @@ player_ratings_master <- bind_rows(
 )
 
 dim(player_ratings_master)
+
+
+library(jsonlite)
+
+json_data <- toJSON(player_ratings_master, pretty = TRUE, auto_unbox = TRUE)
+writeLines(paste0("const playerRatingsMaster = ", json_data, ";"), "player_ratings_master.js")
+
+getwd()
